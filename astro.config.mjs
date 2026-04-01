4644f2b15d7150baf010bf0e072d861aaebd3e64
Jackeline Gonçalves <jackeline.camara04@gmail.com>
2026-04-01 21:41:54 +0100

    netlify configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import netlify from "@astrojs/netlify";
 
 export default defineConfig({
+  adapter: netlify(),
+  output: "static",
   vite: {
     plugins: [tailwindcss()],
   },
